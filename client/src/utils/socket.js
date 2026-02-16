c42ff788f7ee3752f01d2b3b63d8ff68d18e956d
Abhishek Chaudhary <93636606+abhishek-chaudhary2003@users.noreply.github.com>
2026-02-16 08:13:38 +0530

    Add polling transport to socket connection
    Added polling as a fallback transport method for socket.

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -3,6 +3,6 @@ import { io } from "socket.io-client";
 export const socket = io(
   "https://poll-ib3fnwxwj-abhishek10299s-projects.vercel.app",
   {
-    transports: ["websocket"]
+    transports: ["websocket", "polling"] // fallback to polling
   }
 );
